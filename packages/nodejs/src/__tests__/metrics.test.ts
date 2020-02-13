9b32755aa326d534a37ca1f103184534f48ead61
Adam Yeats <xadamy@users.noreply.github.com>
2020-02-13 11:46:23 +0100

    Metrics support (#20)
    * Add metrics support
    
    * Add metrics tests

---
+++ packages/nodejs/src/__tests__/metrics.test.ts
@@ -0,0 +1,27 @@
+import { Metrics } from "../metrics"
+
+describe("Metrics", () => {
+  let metrics: Metrics
+
+  beforeEach(() => {
+    metrics = new Metrics()
+  })
+
+  it("sets a gauge", () => {
+    expect(() => {
+      metrics.setGauge("database_size", 100)
+    }).not.toThrow()
+  })
+
+  it("sets a distribution value", () => {
+    expect(() => {
+      metrics.addDistributionValue("memory_usage", 100)
+    }).not.toThrow()
+  })
+
+  it("increments the counter", () => {
+    expect(() => {
+      metrics.incrementCounter("login_count", 1)
+    }).not.toThrow()
+  })
+})
