e56dc0803ed991618a8106c54fe18edb5605bf7f
Adam Yeats <adam@appsignal.com>
2020-02-26 11:59:58 +0100

    Remove span handling from express plugin

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,36 +1,26 @@
-import * as asyncHooks from "async_hooks"
 import { Appsignal } from "@appsignal/nodejs"
-import { Request, Response, NextFunction, RequestHandler } from "express"
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express"
 
-function expressMiddleware(appsignal: Appsignal): RequestHandler {
-  return function(req: Request, res: Response, next: NextFunction) {
-    const tracer = appsignal.tracer()
-    const rootSpan = tracer.createSpan(`${req.method} ${req.url}`)
+function expressMiddleware(appsignal: Appsignal): ErrorRequestHandler {
+  return function(err: Error, req: Request, res: Response, next: NextFunction) {
+    const span = appsignal.tracer().currentSpan()
 
-    rootSpan.setNamespace("web").set("method", req.method)
+    if (!span) {
+      return next()
+    }
+
+    if (err) {
+      span.addError(err)
+    }
 
     if (req.params.password) {
-      rootSpan.setSampleData("params", {
+      span.setSampleData("params", {
         ...req.params,
         password: "[FILTERED]"
       })
     } else {
-      rootSpan.setSampleData("params", req.params)
+      span.setSampleData("params", req.params)
     }
-
-    req.on("error", (err: Error) => rootSpan.addError(err))
-
-    // HACK! super dirty, but gives the span a scope which is required for
-    // the `ScopeManager` to recall it later (duh).
-    // @TODO: needs a more permanent solution
-    ;(tracer as any)._scopeManager._scopes.set(
-      asyncHooks.executionAsyncId(),
-      rootSpan
-    )
-
-    res.once("finish", () => {
-      rootSpan.close()
-    })
 
     return next()
   }
