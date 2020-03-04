95477cf108a3284622c158a16e319e81c999385d
Adam Yeats <adam@appsignal.com>
2020-03-04 23:35:13 +0100

    Fix express middleware, add error handler

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,16 +1,19 @@
 import { Appsignal } from "@appsignal/nodejs"
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express"
 
-function expressMiddleware(appsignal: Appsignal): ErrorRequestHandler {
-  return function(err: Error, req: Request, res: Response, next: NextFunction) {
+import {
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+  RequestHandler
+} from "express"
+
+function expressMiddleware(appsignal: Appsignal): RequestHandler {
+  return function(req: Request, res: Response, next: NextFunction) {
     const span = appsignal.tracer().currentSpan()
 
     if (!span) {
       return next()
-    }
-
-    if (err) {
-      span.addError(err)
     }
 
     if (req.params.password) {
@@ -26,4 +29,27 @@ function expressMiddleware(appsignal: Appsignal): ErrorRequestHandler {
   }
 }
 
-module.exports = { expressMiddleware }
+function expressErrorHandler(appsignal: Appsignal): ErrorRequestHandler {
+  return function(
+    err: Error & { status?: number },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const span = appsignal.tracer().currentSpan()
+
+    if (!span) {
+      return next()
+    }
+
+    // if there's mo `status` property, forward the error
+    // we also ignore client errors here
+    if (err && (!err.status || (err.status && err.status >= 500))) {
+      span.addError(err)
+    }
+
+    return next(err)
+  }
+}
+
+module.exports = { expressMiddleware, expressErrorHandler }
