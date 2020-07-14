b39fa338837c7c7c1ccfe1cac9b64e5f4d6bbe17
Adam Yeats <adam@appsignal.com>
2020-07-14 16:39:21 +0200

    Refactor Redis plugin, move to core instrumentation

---
+++ packages/nodejs/src/instrumentation/redis/index.ts
@@ -0,0 +1,92 @@
+import shimmer from "shimmer"
+import redis from "redis"
+
+import { Span } from "../../interfaces/span"
+import { Tracer } from "../../interfaces/tracer"
+import { Plugin } from "../../interfaces/plugin"
+
+export const PLUGIN_NAME = "redis"
+
+// quick alias to expose a type for the entire module
+type RedisModule = typeof redis
+
+type RedisCallback = <T>(err: Error | null, reply: T) => void
+
+function wrapCallback(tracer: Tracer, span: Span, done: RedisCallback) {
+  const fn = function <T>(err: Error | null, res: T) {
+    span.close()
+
+    if (done) {
+      done(err, res)
+    }
+  }
+
+  return tracer.wrap(fn)
+}
+
+function createRedisSendCommandWrapper(tracer: Tracer) {
+  return function internalSendCommandWrap(original: Function) {
+    return function wrapRedisSendCommand(
+      this: redis.RedisClient,
+      cmd: { callback: RedisCallback; command: string } | string,
+      args?: any[],
+      cb?: RedisCallback
+    ) {
+      const rootSpan = tracer.currentSpan()
+
+      if (!rootSpan || !cmd) {
+        return original.apply(this, [cmd, args, cb])
+      }
+
+      const span = rootSpan.child().setCategory("command.redis")
+
+      typeof cmd === "string" ? span.setName(cmd) : span.setName(cmd.command)
+
+      if (arguments.length === 1 && typeof cmd === "object") {
+        cmd.callback = wrapCallback(tracer, span, cmd.callback)
+        return original.call(this, cmd)
+      }
+
+      if (
+        !cmd ||
+        !args ||
+        typeof cmd !== "string" ||
+        !Array.isArray(args) ||
+        (cb && typeof cb !== "function")
+      ) {
+        return original.apply(this, [cmd, args, cb])
+      }
+
+      if (!cb) {
+        if (
+          typeof args[args.length - 1] === "function" ||
+          typeof args[args.length - 1] === "undefined"
+        ) {
+          cb = args.pop()
+        }
+      }
+
+      // it's ok to pass `undefined` here, so we override the typecheck for `cb`
+      return original.apply(this, [cmd, args, wrapCallback(tracer, span, cb!)])
+    }
+  }
+}
+
+export const instrument = (
+  mod: RedisModule,
+  tracer: Tracer
+): Plugin<RedisModule> => ({
+  version: ">= 3.0.0",
+  install(): RedisModule {
+    shimmer.wrap(
+      mod.RedisClient.prototype,
+      "internal_send_command",
+      createRedisSendCommandWrapper(tracer)
+    )
+
+    return mod
+  },
+  uninstall(): void {
+    shimmer.unwrap(mod.RedisClient.prototype, "internal_send_command")
+  }
+})
