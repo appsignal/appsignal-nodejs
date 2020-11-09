307505bae3855c6d76ff4cbe3c61eb50ca82fbd8
Victor Tortolero <victor.tortolero@wizeline.com>
2020-11-09 15:58:08 -0600

    Extract function to set span name out of middleware

--- packages/nextjs/src/handler.ts
+++ packages/nextjs/src/handler.ts
@@ -2,20 +2,56 @@ import url from "url"
 import { Appsignal } from "@appsignal/nodejs"
 import { ServerResponse, IncomingMessage } from "http"
 
+interface Route {
+  page: string
+  match: (
+    pathname: string | null | undefined
+  ) => false | { [param: string]: any }
+}
+
 interface NextServer {
   router: {
-    dynamicRoutes?: {
-      page: string
-      match: (
-        pathname: string | null | undefined
-      ) => false | { [param: string]: any }
-    }[]
+    dynamicRoutes?: Route[]
   }
   getRequestHandler(): (
     req: IncomingMessage,
     res: ServerResponse,
     ...rest: any[]
   ) => Promise<void>
+}
+
+export function setSpanName(
+  appsignal: Appsignal,
+  req: IncomingMessage,
+  routes: Route[] = []
+) {
+  const span = appsignal.tracer().currentSpan()
+  const { pathname } = url.parse(req.url || "/", true)
+
+  if (span) {
+    const matched = routes.filter(el => el.match(pathname))[0]
+
+    // passing { debug: true } to the `Appsignal` constructor will log
+    // data about the current route to the console. don't rely on this
+    // working in future!
+    if (appsignal.config.debug) {
+      console.log("[APPSIGNAL]: Next.js debug data", {
+        routes,
+        matched,
+        pathname
+      })
+    }
+
+    if (matched) {
+      // matched to a dynamic route
+      span.setName(`${req.method} ${matched.page}`)
+    } else if (!matched && pathname === "/") {
+      // the root
+      span.setName(`${req.method} ${pathname}`)
+    } else {
+      span.setName(`${req.method} [unknown route]`)
+    }
+  }
 }
 
 /**
@@ -29,35 +65,7 @@ export function getRequestHandler<T extends NextServer>(
   const handler = app.getRequestHandler()
 
   return function (req: IncomingMessage, res: ServerResponse, ...rest: any[]) {
-    const span = appsignal.tracer().currentSpan()
-    const { pathname } = url.parse(req.url || "/", true)
-
-    if (span) {
-      const routes = app.router.dynamicRoutes || []
-      const matched = routes.filter(el => el.match(pathname))[0]
-
-      // passing { debug: true } to the `Appsignal` constructor will log
-      // data about the current route to the console. don't rely on this
-      // working in future!
-      if (appsignal.config.debug) {
-        console.log("[APPSIGNAL]: Next.js debug data", {
-          routes,
-          matched,
-          pathname
-        })
-      }
-
-      if (matched) {
-        // matched to a dynamic route
-        span.setName(`${req.method} ${matched.page}`)
-      } else if (!matched && pathname === "/") {
-        // the root
-        span.setName(`${req.method} ${pathname}`)
-      } else {
-        span.setName(`${req.method} [unknown route]`)
-      }
-    }
-
+    setSpanName(appsignal, req, app.router.dynamicRoutes)
     return handler(req, res, ...rest)
   }
 }
