85d47ea5eb3f0bfe5929686627f945cbead15b1d
Victor Tortolero <victor.tortolero@wizeline.com>
2020-11-09 15:50:25 -0600

    Add option to disable calling setSampleData

--- packages/express/src/middleware/index.ts
+++ packages/express/src/middleware/index.ts
@@ -8,11 +8,23 @@ import {
   RequestHandler
 } from "express"
 
+type ExpressMiddlewareOptions = {
+  /**
+   * Used to indicate if we should call span.setSampleData with
+   * ExpressJS parsed params. If you're setting these manually,
+   * pass false to prevent overriding your values.
+   */
+  recordParams?: boolean
+}
+
 /**
  * Returns an Express middleware that can augment the current span
  * with data.
  */
-export function expressMiddleware(appsignal: Appsignal): RequestHandler {
+export function expressMiddleware(
+  appsignal: Appsignal,
+  { recordParams = false }: ExpressMiddlewareOptions = {}
+): RequestHandler {
   return function (req: Request, res: Response, next: NextFunction) {
     const tracer = appsignal.tracer()
     const rootSpan = tracer.currentSpan()
@@ -41,8 +53,10 @@ export function expressMiddleware(appsignal: Appsignal): RequestHandler {
           span.setName(`${method} ${req.route.path}`)
         }
 
-        // set route params (if parsed by express correctly)
-        span.setSampleData("params", { ...params, ...query })
+        if (recordParams) {
+          // set route params (if parsed by express correctly)
+          span.setSampleData("params", { ...params, ...query })
+        }
 
         return res.end.apply(this, arguments as any)
       }
