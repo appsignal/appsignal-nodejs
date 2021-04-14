c8d1b49210d0e12832b45d89431f3d7b2729f93d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-04-14 14:32:38 +0200

    Add regression test to set span names in Next.js integration

--- packages/nextjs/test/integration_spec.rb
+++ packages/nextjs/test/integration_spec.rb
@@ -1,25 +1,40 @@
 require 'net/http'
+require 'tempfile'
 
 RSpec.describe "Next.js" do
   around do |example|
-    Dir.chdir File.join(__dir__, 'example')
-    read, write = IO.pipe
+    tmpdir = Dir.mktmpdir
+    @log_path = File.join(tmpdir, "appsignal.log")
+    command = "APPSIGNAL_LOG_PATH='#{tmpdir}' APPSIGNAL_DEBUG='true' APPSIGNAL_TRANSACTION_DEBUG_MODE='true' node server.js"
 
-    pid = spawn("node server.js", out: write)
+    Dir.chdir File.join(__dir__, 'example')
+
+    puts command
+    read, write = IO.pipe
+    @pid = spawn(command, out: write)
+
     read.each do |line|
       puts line
       break if line =~ /Ready on/
     end
 
+    uri = URI('http://localhost:3000/')
+
     begin
+      @result = Net::HTTP.get(uri)
       example.run
     ensure
-      Process.kill 3, pid
+      Process.kill 3, @pid
     end
   end
 
   it "renders the index page" do
-    uri = URI('http://localhost:3000/')
-    expect(Net::HTTP.get(uri)).to match(/Welcome to .+Next\.js!/)
+    expect(@result).to match(/Welcome to .+Next\.js!/)
+  end
+
+  it "sets the root span's name" do
+    log = File.read(@log_path)
+    expect(/Start root span '(\w+)' in 'web'/.match(log)).to be_truthy()
+    expect(/Set name 'GET \/' for span '#{$1}'/.match(log)).to be_truthy()
   end
 end
