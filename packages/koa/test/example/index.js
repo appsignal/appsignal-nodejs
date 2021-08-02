aea976a66b0759dd3a29e41512f1ad54fc232854
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-08-02 11:07:31 +0200

    Add integration test to koa example
    The `packages/koa/test/integration_spec.rb` file is based on the
    one from the Next.js tests, with the startup command and expectation
    updated for the Express app.
    
    The test currently fails, as AppSignal is not yet included in the test
    app, meaning the log file isn't created.

--- packages/koa/test/example/index.js
+++ packages/koa/test/example/index.js
@@ -2,7 +2,8 @@ const Koa = require("koa")
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = "Hello World"
+  ctx.body = "Hello World!"
 })
 
 app.listen(3000)
+console.log("Example app listening at http://localhost:3000")
