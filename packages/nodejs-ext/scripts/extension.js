appsignal/appsignal-nodejs

commit bf11c4b801b0dd23537d479c84876b7ce53456c9

Add mirror download support (#440)
The agent was always downloaded from a single url. Adding mirror support
we ensure download availability as we do with Ruby library.

packages/nodejs-ext/scripts/extension.js
@@ -6,7 +6,7 @@ const fs = require("fs")
 const crypto = require("crypto")
 const childProcess = require("child_process")
 
-const { TRIPLES } = require("./extension/constants")
+const { AGENT_VERSION, MIRRORS, TRIPLES } = require("./extension/constants")
 
 const {
   hasLocalBuild,
@@ -25,12 +25,17 @@ const EXT_PATH = path.join(__dirname, "/../ext/")
 const testExtensionFailure =
   process.env._TEST_APPSIGNAL_EXTENSION_FAILURE === "true"
 
-function download(url, outputPath) {
-  return new Promise((resolve, reject) => {
-    if (testExtensionFailure) {
-      throw new Error("AppSignal internal test failure")
-    }
+class DownloadError extends Error {
+  constructor(message, downloadUrl) {
+    super(message)
+    this.name = this.constructor.name
+    this.downloadUrl = downloadUrl
+  }
+}
 
+function downloadFromMirror(mirror, filename, outputPath) {
+  return new Promise((resolve, reject) => {
+    const url = path.join(mirror, AGENT_VERSION, filename)
     const file = fs.createWriteStream(outputPath)
 
     https.get(url, response => {
@@ -38,12 +43,40 @@ function download(url, outputPath) {
 
       if (statusCode >= 400) {
         return reject(
-          new Error(`Request to CDN failed with code HTTP ${statusCode}`)
+          new DownloadError(`Request to CDN failed with code HTTP ${statusCode}`, url)
         )
+      } else {
+        response.pipe(file).on("finish", () => resolve(url))
       }
-
-      response.pipe(file).on("finish", () => resolve(outputPath))
     })
+  })
+}
+
+function download(mirrors, filename, outputPath) {
+  return new Promise((resolve, reject) => {
+    if (testExtensionFailure) {
+      throw new DownloadError("AppSignal internal test failure", undefined)
+    }
+
+    if (mirrors.length === 0) {
+      reject(new DownloadError("Could not download agent from any mirror", undefined))
+      return
+    }
+
+    downloadFromMirror(mirrors.shift(), filename, outputPath)
+      .then(url => {
+        const downloadData = {
+          downloadUrl: url,
+          outputPath: outputPath
+        }
+        resolve(downloadData)
+      })
+      .catch(error => {
+        console.error("Error downloading from mirror:", error)
+        download(mirrors, filename, outputPath)
+          .then(downloadData => resolve(downloadData))
+          .catch(error => reject(error))
+      })
   })
 }
 
@@ -152,27 +185,26 @@ function install() {
 
   // try and get one from the CDN
   const metadata = getMetadataForTarget(report.build)
-  const filename = metadata.downloadUrl.split("/")[4]
+  const filename = metadata.filename
   const outputPath = path.join(EXT_PATH, filename)
 
   report.build.source = "remote"
-  report.download = createDownloadReport({
-    download_url: metadata.downloadUrl
-  })
-  return download(metadata.downloadUrl, outputPath)
-    .then(filepath =>
-      verify(filepath, metadata.checksum).then(() => {
+
+  return download(MIRRORS, filename, outputPath)
+    .then(downloadData => {
+      report.download.download_url = downloadData.downloadUrl
+
+      verify(downloadData.outputPath, metadata.checksum).then(() => {
         report.download.checksum = "verified"
-        return extract(filepath)
+
+        return extract(downloadData.outputPath)
       })
-    )
+    })
     .then(() => {
       // @TODO: add cleanup step
       console.log("The agent has downloaded successfully! Building...")
 
       report.result.status = "unknown"
-
-      // Once extracted, we hand it off to node-gyp for building
       return install().then(() => {
         report.result.status = "success"
       })
@@ -189,6 +221,10 @@ function install() {
         status: "error",
         error: error.message,
         backtrace: error.stack.split("\n")
+      }
+
+      if (error.downloadUrl) {
+        report.download.download_url = error.downloadUrl
       }
 
       return dumpReport(report).then(() => {

packages/nodejs-ext/scripts/extension/constants.js
@@ -1,78 +1,83 @@
 const AGENT_VERSION = "0318770"
 
+const MIRRORS = [
+  "https://appsignal-agent-releases.global.ssl.fastly.net",
+  "https://d135dj0rjqvssy.cloudfront.net"
+]
+
 const TRIPLES = {
   "x86_64-darwin": {
     checksum:
       "7b17cb76edc58ea54381455f74934d08efbfb7807007e97ae01f751101da8b50",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-darwin-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-darwin-all-static.tar.gz"
   },
   "universal-darwin": {
     checksum:
       "7b17cb76edc58ea54381455f74934d08efbfb7807007e97ae01f751101da8b50",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-darwin-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-darwin-all-static.tar.gz"
   },
   "aarch64-darwin": {
     checksum:
       "d90172492ccf83527696fcd0353796d3d0d4e1704ff986ae90a774a7f11a85a2",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-aarch64-darwin-all-static.tar.gz"
+    filename:
+      "appsignal-aarch64-darwin-all-static.tar.gz"
   },
   "arm64-darwin": {
     checksum:
       "d90172492ccf83527696fcd0353796d3d0d4e1704ff986ae90a774a7f11a85a2",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-aarch64-darwin-all-static.tar.gz"
+    filename:
+      "appsignal-aarch64-darwin-all-static.tar.gz"
   },
   "arm-darwin": {
     checksum:
       "d90172492ccf83527696fcd0353796d3d0d4e1704ff986ae90a774a7f11a85a2",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-aarch64-darwin-all-static.tar.gz"
+    filename:
+      "appsignal-aarch64-darwin-all-static.tar.gz"
   },
   "aarch64-linux": {
     checksum:
       "bef06f27d98cc1afc30b2d8fa23af69bd0206407b0d8d2f052278de3b8c5f2b5",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-aarch64-linux-all-static.tar.gz"
+    filename:
+      "appsignal-aarch64-linux-all-static.tar.gz"
   },
   "i686-linux": {
     checksum:
       "7e0aa277c4e49ebe1b805e9db615544c5488a23d8b439867a2a6357d37c897bc",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-i686-linux-all-static.tar.gz"
+    filename:
+      "appsignal-i686-linux-all-static.tar.gz"
   },
   "x86-linux": {
     checksum:
       "7e0aa277c4e49ebe1b805e9db615544c5488a23d8b439867a2a6357d37c897bc",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-i686-linux-all-static.tar.gz"
+    filename:
+      "appsignal-i686-linux-all-static.tar.gz"
   },
   "x86_64-linux": {
     checksum:
       "e918e24ff1f86d939b8f571506b11f2890d81c741de56cb06ac81b5dcc3f70e1",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-linux-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-linux-all-static.tar.gz"
   },
   "x86_64-linux-musl": {
     checksum:
       "1a90421519d7860bf41d606866252cc7f1cb828a7efb9622045ee4f04d757ebd",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-linux-musl-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-linux-musl-all-static.tar.gz"
   },
   "x86_64-freebsd": {
     checksum:
       "22cdd8e44e60dd69003d28ea95c994c27d2223a3872c541c966f32dbea3b0462",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-freebsd-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-freebsd-all-static.tar.gz"
   },
   "amd64-freebsd": {
     checksum:
       "22cdd8e44e60dd69003d28ea95c994c27d2223a3872c541c966f32dbea3b0462",
-    downloadUrl:
-      "https://appsignal-agent-releases.global.ssl.fastly.net/0318770/appsignal-x86_64-freebsd-all-static.tar.gz"
+    filename:
+      "appsignal-x86_64-freebsd-all-static.tar.gz"
   }
 }
 
-module.exports = { AGENT_VERSION, TRIPLES }
+module.exports = { AGENT_VERSION, MIRRORS, TRIPLES }

commit 4e18dc2e2d02c2d13b6012d3a016a05fbcd7522c

Fix checksum verification error handling
During the extension installation we verify if the download's checksum
matches the expected checksum.

When this verification fails, it should write this error to the
installation report, but instead it would not write a report at all.

```
The agent has downloaded successfully! Building...
/integration/packages/nodejs-ext/scripts/extension.js:110
          return reject(new Error("Checksum verification failed"))
                        ^

Error: Checksum verification failed
    at Hash.<anonymous> (/integration/packages/nodejs-ext/scripts/extension.js:110:25)
    at Hash.emit (node:events:388:22)
    at finish (node:internal/streams/writable:741:10)
    at processTicksAndRejections (node:internal/process/task_queues:80:21)
Error: Command failed with status `1`
```

This error should have been caught by the catch statement on line 238,
but wasn't because the promise result wasn't returned from the
`download`'s promise handler that called the `verify` function. Instead
it was not handled at all. You can tell by the output being printed
including: "The agent has downloaded successfully!", indicating it
continued as if no error occurred.

I would have expected an unhandled promise error here actually, but for
whatever reason it just prints the error instead and exits with a
failure like we expect, but without it writing an installation report
file.

This broke in commit bf11c4b801b0dd23537d479c84876b7ce53456c9 (PR #440),
that was released in version 2.1.0. Originally the `verify` function
result was the only thing in the `download`'s promise handler, and was
implicitly returned. It was changed to a multi statement handler that
had no explicit return.

Fixes #455

packages/nodejs-ext/scripts/extension.js
@@ -212,7 +212,7 @@ function install() {
     result = download(MIRRORS, filename, outputPath).then(url => {
       report.download.download_url = url
 
-      verify(outputPath, metadata.checksum).then(() => {
+      return verify(outputPath, metadata.checksum).then(() => {
         report.download.checksum = "verified"
 
         return extract(outputPath)