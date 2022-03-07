0f73ea159502bdaf6541506d0b2298ae979aed37
Noemi <noemi@appsignal.com>
2022-03-07 10:21:16 +0100

    Use ESLint
    Add the ESLint linter to the project, along with the Typescript,
    Prettier and Jest plugins for it, and configure ESLint to use them.
    
    Replace uses of Prettier in the `lint` and `lint:write` actions
    and in the `lint-staged` pre-commit hook with ESLint.
    
    Configure ESLint so that all rules that currently trigger errors
    or warnings are turned off. Subsequent commits will re-enable these
    rules along with the necessary fixes to the code.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es2018: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "prettier", "jest"],
+  rules: {
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "jest/expect-expect": "off",
+    "jest/no-done-callback": "off",
+    "jest/no-identical-title": "off",
+    "no-prototype-builtins": "off",
+    "no-unreachable": "off",
+    "no-useless-escape": "off",
+    "no-var": "off",
+    "prefer-const": "off",
+    "prefer-rest-params": "off"
+  }
+}
