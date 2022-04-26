d961e1e050ce2048f20de55bb24d0c4ad2d65373
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2022-04-26 17:54:44 +0200

    Add SpanProcessor to import OpenTelemetry spans
    Since this patch, the library depends on @opentelemetry/api, to import
    types for HrTime, Span, SpanAttributes and SpanContext. This patch also
    uses the SpanProcessor and ReadableSpan types from
    @opentelemetry/sdk-trace-base, but those are duplicated in
    src/interfaces/span_processor.ts to avoid dependencies.

---
+++ packages/nodejs/src/span_processor.ts
@@ -0,0 +1,42 @@
+import { Span, SpanContext } from "@opentelemetry/api"
+import {
+  OpenTelemetrySpanProcessor,
+  ReadableSpan
+} from "./interfaces/span_processor"
+import { BaseClient as Client } from "./client"
+
+export class SpanProcessor implements OpenTelemetrySpanProcessor {
+  client: Client
+
+  constructor(client: Client) {
+    this.client = client
+  }
+
+  forceFlush(): Promise<void> {
+    return Promise.resolve()
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onStart(_span: Span, _parentContext: SpanContext): void {}
+
+  onEnd(span: ReadableSpan, _parentContext: SpanContext): void {
+    const appsignalSpan = this.client.tracer().currentSpan()
+
+    this.client.extension.importOpenTelemetrySpan(
+      span.spanContext().spanId,
+      appsignalSpan.spanId,
+      appsignalSpan.traceId,
+      span.startTime[0],
+      span.startTime[1],
+      span.endTime[0],
+      span.endTime[1],
+      span.name,
+      span.attributes,
+      span.instrumentationLibrary.name
+    )
+  }
+
+  shutdown(): Promise<void> {
+    return Promise.resolve()
+  }
+}
