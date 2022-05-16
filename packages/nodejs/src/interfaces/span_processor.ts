ee1ea8b7bcf557c2e236a234181fa461365f3071
Tom de Bruijn <tom@tomdebruijn.com>
2022-05-16 11:40:53 +0200

    Import OpenTelemetry types rather than duplicate (#659)
    Use Typescript's `import type` and `export type` to include the
    OpenTelemetry types into our package without having to copy the code
    into our project directly.
    
    This was suggested by @unflxw in PR
    https://github.com/appsignal/appsignal-nodejs/pull/651#discussion_r866916531
    
    I'm not sure if this is more or less fragile. If the OpenTelemetry
    package changes the interface we'd be shipping an older version that
    doesn't work with older versions anymore.
    
    The package from which we import is a dev dependency only, and used
    during the compile step, and won't ship as a dependency in the final
    release.
    
    I got an issue when using the OpenTelemetry SpanProcessor type for the
    `onEnd` function. It doesn't have a second argument.
    
    The `onStart` also needed another type, from `SpanContext` to `Context`.

--- packages/nodejs/src/interfaces/span_processor.ts
+++ packages/nodejs/src/interfaces/span_processor.ts
@@ -1,44 +1,7 @@
-import { HrTime, Span, SpanAttributes, SpanContext } from "@opentelemetry/api"
+import type {
+  Span,
+  ReadableSpan,
+  SpanProcessor
+} from "@opentelemetry/sdk-trace-base"
 
-/**
- * OpenTelemetrySpanProcessor is based on OpenTelemetry's internal span processor.
- */
-export interface OpenTelemetrySpanProcessor {
-  /**
-   * Forces to export all finished spans
-   */
-  forceFlush(): Promise<void>
-
-  /**
-   * Called when an OpenTelemetry {@link Span} is started, if the
-   * `span.isRecording()` returns true.
-   * @param span the OpenTelemetry Span that just started.
-   */
-  onStart(_span: Span, _parentContext: SpanContext): void
-
-  /**
-   * Called when an OpenTelemetry {@link Span} is ended, if the
-   * `span.isRecording()` returns true.
-   * @param span the Span that just ended.
-   */
-  onEnd(_span: ReadableSpan, _parentContext: SpanContext): void
-
-  /**
-   * Shuts down the processor. Called when the OpenTelemetry SDK is shut down.
-   * This is an opportunity for processor to do any cleanup required.
-   */
-  shutdown(): Promise<void>
-}
-
-/**
- * ReadableSpan is based on a subset of OpenTelemetry's internal "readable"
- * spans, which have readonly attributes for exporting.
- */
-export interface ReadableSpan {
-  readonly name: string
-  readonly spanContext: () => SpanContext
-  readonly startTime: HrTime
-  readonly endTime: HrTime
-  readonly attributes: SpanAttributes
-  readonly instrumentationLibrary: { name: string }
-}
+export type { Span, ReadableSpan, SpanProcessor }
