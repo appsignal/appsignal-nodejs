7b5f4e5844e18979e1da96c50ef2054e7d76b543
Tom de Bruijn <tom@tomdebruijn.com>
2022-05-25 15:49:51 +0200

    Remove AppSignal requirement for SpanProcessor
    For integrations like Express we'll rely on the OpenTelemetry
    instrumentations to create root spans.
    
    AppSignal instrumentation and OpenTelemetry instrumentation is not
    compatible with each other this way. This change changes the requirement
    from an AppSignal instrumentation to the OpenTelemetry instrumentation.
    
    If not, the parentSpanId and traceId of the AppSignal span is empty and
    it will create incidents for all the separate child spans.
    
    Part of https://github.com/appsignal/opentelemetry/issues/14
    Requires https://github.com/appsignal/appsignal-agent/pull/762

--- packages/nodejs/src/span_processor.ts
+++ packages/nodejs/src/span_processor.ts
@@ -5,7 +5,6 @@ import {
   SpanProcessor as OpenTelemetrySpanProcessor
 } from "./interfaces/span_processor"
 import { BaseClient as Client } from "./client"
-import { NoopSpan } from "./noops"
 
 export class SpanProcessor implements OpenTelemetrySpanProcessor {
   client: Client
@@ -22,22 +21,18 @@ export class SpanProcessor implements OpenTelemetrySpanProcessor {
   onStart(_span: Span, _parentContext: Context): void {}
 
   onEnd(span: ReadableSpan): void {
-    const appsignalSpan = this.client.tracer().currentSpan()
-
-    if (!(appsignalSpan instanceof NoopSpan)) {
-      this.client.extension.importOpenTelemetrySpan(
-        span.spanContext().spanId,
-        appsignalSpan.spanId,
-        appsignalSpan.traceId,
-        span.startTime[0],
-        span.startTime[1],
-        span.endTime[0],
-        span.endTime[1],
-        span.name,
-        span.attributes,
-        span.instrumentationLibrary.name
-      )
-    }
+    this.client.extension.importOpenTelemetrySpan(
+      span.spanContext().spanId,
+      span.parentSpanId || "",
+      span.spanContext().traceId,
+      span.startTime[0],
+      span.startTime[1],
+      span.endTime[0],
+      span.endTime[1],
+      span.name,
+      span.attributes,
+      span.instrumentationLibrary.name
+    )
   }
 
   shutdown(): Promise<void> {
