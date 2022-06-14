79817aaea96d2471779e36b6414e5c8d05b4f3c2
Tom de Bruijn <tom@tomdebruijn.com>
2022-06-14 07:57:57 +0200

    Remove all integration packages
    We're going the OpenTelemetry only route.
    
    - Removed packages:
      - apollo-server
      - express
      - nextjs
      - koa
    - Update build matrix to match
    - Add shimmer types to dev dependencies
      - Otherwise the build fails. This was previously a dev dependency of
        the koa package but removing that package causes the build step to
        fail.
    
    Written by (I'm just reapplying):
    
    Co-authored-by: Noemi <noemi@appsignal.com>
    Co-authored-by: Luismi Ramirez <luismi.ramirez@protonmail.com>

--- packages/nextjs/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./handler"
-import * as experimental from "./experimental"
-export const EXPERIMENTAL = experimental
--- packages/nextjs/test/example/pages/blog/index.js
+++
@@ -1,3 +0,0 @@
-export default function BlogIndexPage() {
-  return <h1>Blog</h1>
-}
