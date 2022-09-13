a8521465243e2e6f3ca4eac14ca411422cb8e1dc
Luismi Ramírez <luismi.ramirez@protonmail.com>
2022-09-13 12:39:56 +0200

    Test request parameters presence
    A new custom matcher that checks for the presence of the given
    parameters in the request parameters magic attribute.

--- test/express-redis/tests/spec/app_spec.rb
+++ test/express-redis/tests/spec/app_spec.rb
@@ -5,6 +5,22 @@ RSpec.describe "Redis app" do
     @test_app_url = ENV.fetch("TEST_APP_URL")
   end
 
+  describe "GET / with params" do
+    it "adds params to the request handler span" do
+      response = HTTP.get("#{@test_app_url}?param1=user&param2=password")
+      expect(response.status).to eq(200)
+      expect(Span.root!).to be_http_span_with_route("GET /")
+
+      request_handler_span = Span.find_by_name!("request handler - /")
+      expected_request_parameters = {
+        "param1" => "user",
+        "param2" => "password"
+      }
+
+      expect(request_handler_span).to match_request_parameters(expected_request_parameters)
+    end
+  end
+
   describe "GET /ioredis" do
     it "creates an IORedis child span on the HTTP root span" do
       response = HTTP.get("#{@test_app_url}/ioredis")
--- test/helpers/http_helper.rb
+++ test/helpers/http_helper.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 require "rspec/expectations"
+require "json"
 
 RSpec::Matchers.define :be_http_span_with_route do |expected|
   match do |actual|
     actual.name == expected &&
       actual.instrumentation_library_name == "@opentelemetry/instrumentation-http"
+  end
+end
+
+RSpec::Matchers.define :match_request_parameters do |expected|
+  match do |actual|
+    parsed_params = JSON[actual.attributes["appsignal.request.parameters"]]
+
+    parsed_params == expected
   end
 end
