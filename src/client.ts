1cb782079fdfe6b857e4187414e30bb81d5ee30e
Tom de Bruijn <tom@tomdebruijn.com>
2023-01-10 13:48:46 +0100

    Change Client.tracer to getter function
    Change to a getter so the error is triggered when `Client.tracer` is
    called, and not only later when the function is called
    `Client.tracer()`.
    
    The getter needs to return a value from TypeScript, so it will now
    return an empty function to mimic the function it was previously.

--- src/client.ts
+++ src/client.ts
@@ -387,9 +387,10 @@ export class Client {
     global.__APPSIGNAL__ = this
   }
 
-  tracer(): void {
+  get tracer(): any {
     console.error(
       "The `appsignal.tracer()` function was called, but it has been removed in AppSignal for Node.js package version 3.x. Please read our migration guide to upgrade to this new version of our package: https://docs.appsignal.com/nodejs/3.x/migration-guide.html. It is also possible to downgrade to version 2.x, after which this code will work again."
     )
+    return () => {}
   }
 }
