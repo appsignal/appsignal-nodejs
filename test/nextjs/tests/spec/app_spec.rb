c01b5aab4372f7cf46d990a48464e03b4ca49719
Noemi <45180344+unflxw@users.noreply.github.com>
2023-04-03 12:06:29 +0200

    Adapt HTTP and Postgres span expectations
    The HTTP spans no longer start with "HTTP" (so, instead of "HTTP GET",
    it just says "GET") and the HTTP spans wrapping the Apollo and Yoga
    spans now have `POST /graphql` in their span name. This seems like a
    good change.
    
    The PostgreSQL spans now have the name of the DB after the span
    name, and their name more closely resembles a span category. In
    addition, there are now spans that represent the connection. We
    should amend the extractor to better deal with these spans, but it's
    okay for now.

--- test/nextjs/tests/spec/app_spec.rb
+++ test/nextjs/tests/spec/app_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "NextJS app" do
     it "creates an HTTP root span" do
       response = HTTP.get("#{@test_app_url}/api/hello")
       root_span = Span.find_by_attribute("http.target", "/api/hello")
-      expect(root_span).to be_http_span_with_route("HTTP GET")
+      expect(root_span).to be_http_span_with_route("GET")
       expect(response.status).to eq(200)
     end
   end
