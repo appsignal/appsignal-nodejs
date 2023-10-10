c70061e3ba1fb1a1a5b056719202853c67611c45
Tom de Bruijn <tom@tomdebruijn.com>
2023-10-10 15:24:36 +0200

    Fix Next.js test app assertion
    Updating Next.js adds the path of the route to the span name. This isn't
    a problem for us. We only use the span name as a fallback, and our agent
    tests already assume it can contain the route path.
    
    It also now is reported by the `next.js` instrumentation, rather than
    the default http one, but we need it to report one from the `next.js`
    integration, so I'm not sure how it worked before.

--- test/helpers/http_helper.rb
+++ test/helpers/http_helper.rb
@@ -3,9 +3,9 @@
 require "rspec/expectations"
 require "json"
 
-RSpec::Matchers.define :be_http_span_with_route do |expected|
+RSpec::Matchers.define(:be_http_span_with_route) do |expected, library = "@opentelemetry/instrumentation-http"| # rubocop:disable Layout/LineLength
   match do |actual|
     actual.name == expected &&
-      actual.instrumentation_library_name == "@opentelemetry/instrumentation-http"
+      actual.instrumentation_library_name == library
   end
 end
