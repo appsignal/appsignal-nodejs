883c8ed67a106ff93f6463d7f2dd6c9e8fd687f1
Tom de Bruijn <tom@tomdebruijn.com>
2023-12-04 12:31:20 +0100

    Update test apps Node.js to fix permissions issue
    For some reason the Node.js 16 container broke with the error message
    below.
    
    Update the Node.js version to 18 resolves the issue. I've also removed
    the installation of specific npm versions. It appears to work with the
    default installed version npm v8.
    
    Change the netcat install to netcat-openbsd as that's the package name
    on this version of Debian.
    
    ```
    npm ERR! code EACCES
    npm ERR! syscall mkdir
    npm ERR! path /root/.npm/_cacache/tmp
    npm ERR! errno -13
    npm ERR!
    npm ERR! Your cache folder contains root-owned files, due to a bug in
    npm ERR! previous versions of npm which has since been addressed.
    npm ERR!
    npm ERR! To permanently fix this problem, please run:
    npm ERR!   sudo chown -R 1001:1001 "/root/.npm"
    ```

--- test/express-prisma-mongo/app/run.sh
+++ test/express-prisma-mongo/app/run.sh
@@ -13,9 +13,6 @@ export DATABASE_URL="mongodb://$DATABASE_HOST:27017/prisma"
 echo "Giving permission to all users on span directory"
 chmod -R 777 /spans
 
-echo "Installing compatible NPM version"
-npm install -g npm@7.18.1
-
 echo "Install, link and build integration"
 (
 cd /integration
