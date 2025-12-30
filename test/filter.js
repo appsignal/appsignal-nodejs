b3b5e1385bc20b64b7e3c42df99827500a94309f
Martin Lipskis <lipskis@users.noreply.github.com>
2025-12-30 17:15:06 +0200

    Update Jest test path filter function
    In Jest v30, the [filter function is expected to return `{ filtered:
    Array<string> }`](https://jestjs.io/docs/cli#--filterfile).
    
    Update the filter function and use `.endsWith` instead of `.indexOf`
    string methods to filter tests with expected failures.

--- test/filter.js
+++ test/filter.js
@@ -5,21 +5,16 @@ const testExtensionFailure =
 // This function is run by using the --filter option for Jest. It will filter
 // out tests that should not be run in certain situations, like testing the
 // scenario when the extension failed.
-module.exports = function (tests) {
+module.exports = testPaths => {
   return {
-    filtered: tests
-      .filter(t => {
-        const match = t.indexOf(".failure")
-        if (testExtensionFailure) {
-          // Select tests with `.failure` in the path if we're testing the
-          // failure state.
-          return match > -1
-        } else {
-          // Skip the tests with `.failure` in the path if we're testing the
-          // success state.
-          return match == -1
-        }
-      })
-      .map(test => ({ test }))
+    filtered: testPaths.filter(path => {
+      if (
+        path.endsWith(".failure.test.js") ||
+        path.endsWith(".failure.test.ts")
+      ) {
+        return testExtensionFailure
+      }
+      return true
+    })
   }
 }
